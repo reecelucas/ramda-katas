947f0909f1f5ec7609fd07d81377e2c7137f5122
Reece Lucas <reece.lucas@bbc.co.uk>
2019-03-12 09:29:25 +0000

    Comment parseQueryString Ramda example

--- katas/parseQueryString/ramda.js
+++ katas/parseQueryString/ramda.js
@@ -2,10 +2,10 @@ const R = require('ramda');
 
 const queryString = '?page=2&pageSize=10&total=203';
 const parseQueryString = R.pipe(
-  R.tail,
-  R.split('&'),
-  R.map(R.split('=')),
-  R.fromPairs
+  R.tail, // Remove the '?'
+  R.split('&'), // ['page=2', 'pageSize=10'...]
+  R.map(R.split('=')), // [['page', '2'], ['pageSize', '10']...]
+  R.fromPairs // final object
 );
 
 const result = parseQueryString(queryString);
